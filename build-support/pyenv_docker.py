8abefbd32cc31c8b497da1bbda798194a307b521
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-08-14 01:51:56 +0200

    collapse some run commands

--- build-support/pyenv_docker.py
+++ build-support/pyenv_docker.py
@@ -27,13 +27,13 @@ class PyenvDockerfileGenerator:
                 f"""
                 FROM {self.source_stage} as {self.temp_stage_prefix}-base
                 ENV DEBIAN_FRONTEND noninteractive
+                ENV PYENV_ROOT /root/.pyenv
+                ENV PATH $PATH:$PYENV_ROOT/shims:$PYENV_ROOT/bin
                 RUN apt-get update && apt-get install -y curl git openssh-client \\
                     build-essential libssl-dev zlib1g-dev libbz2-dev \\
                     libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \\
-                    xz-utils tk-dev libffi-dev liblzma-dev
-                ENV PYENV_ROOT /root/.pyenv
-                ENV PATH $PATH:$PYENV_ROOT/shims:$PYENV_ROOT/bin
-                RUN curl https://pyenv.run | bash
+                    xz-utils tk-dev libffi-dev liblzma-dev \\
+                    && curl https://pyenv.run | bash
                 """
             ).strip()
         )
@@ -67,8 +67,7 @@ class PyenvDockerfileGenerator:
         print(f"FROM {self.temp_stage_prefix}-base as {self.result_stage}")
         # for stage_name in stages:
         #     print(f"COPY --from={stage_name} $PYENV_ROOT $PYENV_ROOT")
-        print("RUN pyenv update")
-        print(f"RUN pyenv global {self.default_python_version}")
+        print("RUN pyenv update && pyenv global {self.default_python_version}")
 
         return fp.getvalue()
 
