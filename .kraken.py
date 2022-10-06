a1d22993717dfee5755b6936f33226d3ba9768a0
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-10-06 13:07:14 +0200

    build for ubuntu_bionic instead

--- .kraken.py
+++ .kraken.py
@@ -16,7 +16,7 @@ project = Project.current()
 version = GitVersion.parse(git_describe(project.directory)).format(dirty=False)
 image_prefix = "ghcr.io/kraken-build/kraken-base-image"
 default_base_image = "ubuntu:focal"
-base_images = {default_base_image, "debian:bullseye"}
+base_images = {default_base_image, "ubuntu:bionic"}
 platforms = ["linux/arm64", "linux/amd64"]
 python_versions = ["3.6.15", "3.7.13", "3.8.13", "3.9.12", "3.10.4", "3.11-dev"]
 global_python_version = "3.10.4"
