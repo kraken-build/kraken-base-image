1119c6d63cfcf77cf1f3b8756fdfaf48c519873c
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-10-15 19:19:46 +0200

    fix terraform arch

--- formulae/terraform.py
+++ formulae/terraform.py
@@ -7,7 +7,7 @@ class TerraformFormula(BinaryInstallFormula):
 
     platform = {"linux": "unknown-linux-musl", "darwin": "apple-darwin"}[sys.platform]
     version = "1.2.7"
-    archive_url = "https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_${arch}.zip"
+    archive_url = "https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_${archv2}.zip"
     archive_members = ["terraform"]
     install_to = "/usr/local/bin"
 
