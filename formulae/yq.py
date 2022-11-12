b1f7244b066b0a0d0eeb70c25de5ef509283c618
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-11-12 14:28:41 +0000

    fix yq formula

--- formulae/yq.py
+++ formulae/yq.py
@@ -9,5 +9,5 @@ class YqFormula(DownloadFileFormula):
     version = "4.30.1"
     download_url = "https://github.com/mikefarah/yq/releases/download/v${version}/yq_${platform}_${archv2}"
     chmod = 0o775
-    output_directory = "${install_to}"
+    output_file = "${install_to}/yq"
     install_to = "/usr/local/bin"
