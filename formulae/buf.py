f91491c2081828986e227aadfe224f0dfda15ede
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-01-18 00:22:57 +0100

    Draft: Configure image to run as non-root by default

---
+++ formulae/buf.py
@@ -0,0 +1,9 @@
+from formula import DownloadFileFormula
+
+
+class BufFormula(DownloadFileFormula):
+
+    version = "1.17.0"
+    download_url = "https://github.com/bufbuild/buf/releases/download/v${version}/buf-Linux-{archv1}"
+    output_file = "/usr/local/bin/buf"
+    chmod = 0o755
