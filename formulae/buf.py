2adb599e65bbddf13fa867d0e7ef60542bc31172
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-01-18 00:29:52 +0100

    fix buf download url template

--- formulae/buf.py
+++ formulae/buf.py
@@ -4,6 +4,6 @@ from formula import DownloadFileFormula
 class BufFormula(DownloadFileFormula):
 
     version = "1.17.0"
-    download_url = "https://github.com/bufbuild/buf/releases/download/v${version}/buf-Linux-{archv1}"
+    download_url = "https://github.com/bufbuild/buf/releases/download/v${version}/buf-Linux-${archv1}"
     output_file = "/usr/local/bin/buf"
     chmod = 0o755
