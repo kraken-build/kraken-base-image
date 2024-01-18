b8a1ccb7958ed5690cbc2cea6f167165a5e1634a
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-01-18 21:38:35 +0100

    fix output_file template

--- formulae/buf.py
+++ formulae/buf.py
@@ -6,5 +6,5 @@ class BufFormula(DownloadFileFormula):
     version = "1.17.0"
     download_url = "https://github.com/bufbuild/buf/releases/download/v${version}/buf-Linux-${archv1}"
     install_to = "/usr/local/bin"
-    output_file = "{install_to}/buf"
+    output_file = "${install_to}/buf"
     chmod = 0o755
