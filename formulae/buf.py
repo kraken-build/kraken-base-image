24137ffed3a81a013898ebacb22765a86b1bc202
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-01-18 01:04:06 +0100

    respect install_to in BufFormula

--- formulae/buf.py
+++ formulae/buf.py
@@ -5,5 +5,5 @@ class BufFormula(DownloadFileFormula):
 
     version = "1.17.0"
     download_url = "https://github.com/bufbuild/buf/releases/download/v${version}/buf-Linux-${archv1}"
-    output_file = "/usr/local/bin/buf"
+    output_file = "{install_to}/buf"
     chmod = 0o755
