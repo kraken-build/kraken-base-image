d26c8a5a0558d2dd4894e6dceac0feaf1417f30c
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-03-07 17:15:36 +0100

    fix cri-dockerd formula

--- formulae/cri-dockerd.py
+++ formulae/cri-dockerd.py
@@ -5,5 +5,5 @@ class CriDockerdFormula(BinaryInstallFormula):
 
     version = "0.3.10"
     archive_url = "https://github.com/Mirantis/cri-dockerd/releases/download/v${version}/cri-dockerd-${version}.amd64.tgz"
-    archive_members = ["cri-dockerd"]
+    archive_members = ["cri-dockerd/cri-dockerd"]
     install_to = "/usr/local/bin"
