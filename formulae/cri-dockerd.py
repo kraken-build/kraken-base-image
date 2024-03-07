9032c12b649c93359ec6ce19005d184ae6434d59
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-03-07 17:09:29 +0100

    fix formuls

--- formulae/cri-dockerd.py
+++ formulae/cri-dockerd.py
@@ -4,6 +4,6 @@ from formula import BinaryInstallFormula
 class CriDockerdFormula(BinaryInstallFormula):
 
     version = "0.3.10"
-    download_url = "https://github.com/Mirantis/cri-dockerd/releases/download/v${version}/cri-dockerd-${version}.amd64.tgz"
+    archive_url = "https://github.com/Mirantis/cri-dockerd/releases/download/v${version}/cri-dockerd-${version}.amd64.tgz"
     archive_members = ["cri-dockerd"]
     install_to = "/usr/local/bin"
--- formulae/crictl.py
+++ formulae/crictl.py
@@ -4,6 +4,6 @@ from formula import BinaryInstallFormula
 class CrictlFormula(BinaryInstallFormula):
 
     version = "v1.29.0"
-    download_url = "https://github.com/kubernetes-sigs/cri-tools/releases/download/${version}/crictl-${version}-linux-amd64.tar.gz"
+    archive_url = "https://github.com/kubernetes-sigs/cri-tools/releases/download/${version}/crictl-${version}-linux-amd64.tar.gz"
     archive_members = ["crictl"]
     install_to = "/usr/local/bin"
