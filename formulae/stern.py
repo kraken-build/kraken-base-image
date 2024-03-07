0214570cc8b31d0d8ac40f39f3956df7cec531c8
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-03-07 18:29:44 +0100

    fix

--- formulae/stern.py
+++ formulae/stern.py
@@ -4,6 +4,6 @@ from formula import BinaryInstallFormula
 class SternFormula(BinaryInstallFormula):
 
     version = "1.28.0"
-    archive_url = "https://github.com/stern/stern/releases/download/v${version}/stern_${versio}_linux_${archv2}.tar.gz"
+    archive_url = "https://github.com/stern/stern/releases/download/v${version}/stern_${version}_linux_${archv2}.tar.gz"
     archive_members = ["stern"]
     install_to = "/usr/local/bin"
