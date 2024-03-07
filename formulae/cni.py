c764540506a593cb85f3f85c3e205f2569715bd6
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-03-07 17:36:06 +0100

    install CNI and update readme

---
+++ formulae/cni.py
@@ -0,0 +1,10 @@
+from formula import BinaryInstallFormula
+
+
+class CniFormula(BinaryInstallFormula):
+
+    version = "v1.4.0"
+    archive_url = "https://github.com/containernetworking/plugins/releases/download/${verison}/cni-plugins-linux-${archv2}-${version}.tgz"
+    archive_members = ["*"]
+    install_to = "/opt/cni/bin"
+    strip_all_components = False
